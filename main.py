db77f072bc2ae06c7747f22c9be9f67719564607
brunoharlein <bruno@harlein.com>
2019-11-07 11:38:03 +0100

    add music with pygame and display start programe text

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3.7
 # coding: utf8
 
-import time #import time for timing break
 
 from functions import * #import function in functions file
+
+# start import
+import pygame #import pygame for play mp3 sound
+import time #import time for timing
+# end import
+
+#start music with pygame and display text
+pygame.init()
+pygame.mixer.music.load("lord.mp3") #load sound
+pygame.mixer.music.play() #play sound
+time.sleep(2) #2 seconds before start game
+print("Dans quelques secondes le programme commencera")
+time.sleep(4)
+print("Ceci est le meilleur jeu du monde")
+time.sleep(3)
+print("Il est temps de rejoindre Golum")
+time.sleep(3)
+#end music program
 
 #score user1 and computer in dictionary
 score = {
@@ -13,4 +30,5 @@ score = {
 
 #start program
 #ask name player
-playerName()
+user1 = playerName()
+
